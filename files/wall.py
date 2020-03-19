5f66e7bfd082b1d5709e7f8e9954b5544aa736e3
Haapaniemi Aarni <aarni.haapaniemi@aalto.fi>
2020-03-19 15:58:03 +0200

    updated walls

--- files/wall.py
+++ files/wall.py
@@ -1,14 +1,14 @@
 class Wall:
-    def __init__(self, between):
-        self.between = between
-        self.active = None
+    def __init__(self, coords, orientation):
+        self.coords = coords
+        self.orientation = orientation  # 0 = vertical, 1 = horizontal
+        self.active = True
 
-    def get_surrounding_squares(self):
-        """ eg (10.5, 4)"""
-        b = self.between
-        if isinstance(b[0], float):
-            return [[int(b[0]), b[1]], [[int(b[0]) + 1, b[1]]]]
-        return [[b[0], int(b[1])], [[b[0], int(b[1]) + 1]]]
+    def get_activity(self):
+        return self.active
 
     def set_active(self):
         self.active = True
+
+    def set_inactive(self):
+        self.active = False
