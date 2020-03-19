a8df00552690b3589df1ff397a85cea10bc90ab1
Haapaniemi Aarni <aarni.haapaniemi@aalto.fi>
2020-03-19 15:57:16 +0200

    can now construct maze

--- files/maze.py
+++ files/maze.py
@@ -1,26 +1,48 @@
-from random import randint
+from random import choice
 
 
 def construct_maze(grid):
+    """
+    edits grid('s walls) to make a maze
+    using randomised prim's algorithm
+    """
     max_x = grid.get_width()
     max_y = grid.get_heigth()
-    while True:
-        square = grid.get_inactive_square()
-        coords = square.get_coords()
-        grid.set_active(coords[0], coords[1])
+    square = grid.get_inactive_square()
+    square.set_active()
+    coords = square.get_coords()
+    c_x = coords[0]
+    c_y = coords[1]
+    inactive_neighbours = [i for i in grid.get_inactive_neighbours(c_x, c_y)]
+    while inactive_neighbours:  # inactive_neighbours not empty
+        new_square = choice(inactive_neighbours)
+        inactive_neighbours.remove(new_square)
+        c_x = new_square.get_coords()[0]
+        c_y = new_square.get_coords()[1]
+        new_inactives = grid.get_inactive_neighbours(c_x, c_y)
 
+        for neighbor in new_inactives:
+            if neighbor not in inactive_neighbours:
+                inactive_neighbours.append(neighbor)
+
+        new_square.set_active()
+        active_neighbours = grid.get_active_neighbours(c_x, c_y)
+        passage_to = choice(active_neighbours)
+
+
+
+
+
+
+"""
         if not square:
             return
         n = randint(0, 3)  # 0 = top, 1 = left, 2 = right, 3 = bottom
 
         if n == 0:
-
-            can_move_up = True
-            if coords[0] < 1:
-                can_move_up = False
-
-            if can_move_up:
-                other_square = [coords[0] - 1, coords[1]]
+            if coords[0] > 0:  # can move up
+                # other_square_coords = [coords[0] - 1, coords[1]]
                 other_square_active = grid.is_square_active(coords[0] - 1, coords[1])
-                if other_square_active:
-                    pass
+                if not other_square_active:
+                    grid.set_active(coords[0] - 1, coords[1])
+"""
