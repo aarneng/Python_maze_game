a6174fa751fddf845d195c68dd2c7deaae664023
Haapaniemi Aarni <aarni.haapaniemi@aalto.fi>
2020-03-19 15:57:38 +0200

    Replace square.py

--- files/square.py
+++ files/square.py
@@ -1,3 +1,6 @@
+from grid import Walls
+
+
 class Square:
     def __init__(self, coords):  # , walls):
         self.coords = coords
@@ -15,3 +18,21 @@ class Square:
 
     def set_active(self):
         self.active = True
+
+    def remove_wall_between(self, other_square):
+
+        other_x = other_square.get_coords()[0]
+        other_y = other_square.get_coords()[1]
+
+        if self.coords[0] - other_x == 0:
+            walls = Walls().get_horizontal()
+            if self.coords[1] - other_y == 1:
+                walls[other_x][other_y + 1].set_inactive()
+            else:
+                walls[other_x][other_y].set_inactive()
+        else:
+            walls = Walls().get_vertical()
+            if self.coords[0] - other_x == 1:
+                walls[other_x + 1][other_y].set_inactive()
+            else:
+                walls[other_x][other_y].set_inactive()
